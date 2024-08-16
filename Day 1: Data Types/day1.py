66d5f4b9a69d45fc9076a08ab1779bfaca1d7bea
VIJAY <93029132+VJLIVE@users.noreply.github.com>
2024-08-16 19:30:47 +0530

    Create day1.py

---
+++ Day 1: Data Types/day1.py
@@ -0,0 +1,10 @@
+#i = 4
+#d = 4.0
+#s = 'HackerRank '
+# The above three codes are already given in the line
+a = int(input())
+b = float(input())
+c = str(input())
+print(a+i)
+print(b+d)
+print(s+c)
