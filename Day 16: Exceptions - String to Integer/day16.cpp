3b5b78a5e8f5b085a5db513e5f60fc02f3e370f9
VIJAY <93029132+VJLIVE@users.noreply.github.com>
2024-09-01 19:24:30 +0530

    Create day16.cpp

---
+++ Day 16: Exceptions - String to Integer/day16.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    try {
+        string str;
+        cin >> str;
+        int num = stoi(str);
+        cout << num;
+    }
+    catch (...) {
+        cout << "Bad String";
+    }
+    return 0;
+}
